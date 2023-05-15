421eb0a4e5c70fe8aa22b58e18dc02d19fe65710
Abraam <abraamaj27@gmail.com>
2023-05-15 20:47:24 +0300

    Modified

--- server-side/Models/CourseModel.js
+++
@@ -1,9 +0,0 @@
-const DB_Connection = require("../DB_Connection");
-var CourseSchema = {
-    courseName:{type:String,pattern:"^[a-zA-Z]+$", required:true},
-    courseDuration:{ type:Number, max:30 },
-}
-var Course = DB_Connection.model("Courses",CourseSchema);
-
-
-module.exports = Course;
